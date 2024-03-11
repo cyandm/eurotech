44b4e096d4e5cf56b7214943755e69b3b81c36d0
lili <pourbabalili@gmail.com>
2024-03-11 16:20:27 +0330

    first commit v1.0.0.0

---
+++ product.php
@@ -0,0 +1,1 @@
+<?php get_header();  /*Template Name: product */ ?>rgg
---
+++ templates/archive/product.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main class="product">
+    <div class="banner" <?php printf("style=\"background-image:url('%s')\"", $banner) ?>>
+        <p>Our doors add beauty and elegance to your office</p>
+    </div>
+    <div class="container">
+    </div>
+</main>
