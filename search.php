d688eb314c79818ff741535bb4be3b2e4ff08f8c
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2024-03-18 15:06:08 +0330

    blogs fixed in search

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php ?>
 <?php
-$banner = get_the_post_thumbnail_url($post_id, 'full');
-
+$front_page_id = get_option('page_on_front');
+$banner = get_field('search_hero_image', $front_page_id);
 
 $search_query = get_search_query();
 
@@ -21,32 +21,19 @@ $query_product_args = [
 ];
 $query_product = new WP_Query($query_product_args);
 
-?>
-<?php
-
-//global $wp_query;
-
-// $searchQuery = get_search_query();
-// $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-
-// if (isset($_GET['section']) && $_GET['section'] == 'blog') {
-//     $section = "blog";
-// } elseif (isset($_GET['section']) && $_GET['section'] == 'product') {
-//     $section = "product";
-// } else {
-//     $section = "all";
-// }
-
 
 ?>
 
 <?php get_header() ?>
 <main class="search-result-page">
     <div class="banner" <?php printf("style=\"background-image:url('%s')\"", $banner) ?>>
-        <p>
-            <?= get_field("search_title"); ?>
+        <p class="container">
+            <?= get_field("search_title", $front_page_id); ?>
         </p>
     </div>
+    <p class="container title-mobile-search">
+        <?= get_field("search_title", $front_page_id); ?>
+    </p>
     <div class="search-result container">
         <?php if (($query_product->have_posts()) || ($query_blog->have_posts())) : ?>
 
@@ -55,7 +42,7 @@ $query_product = new WP_Query($query_product_args);
                     <h2>
                         Search results in products for <span>"<?php echo  get_search_query();  ?> "</span>
                     </h2>
-                    <div class="product-desktop">
+                    <div class="products">
                         <?php
                         if ($query_product->have_posts()) {
                             while ($query_product->have_posts()) {
@@ -66,17 +53,7 @@ $query_product = new WP_Query($query_product_args);
                         wp_reset_postdata();
                         ?>
                     </div>
-                    <div class="product-mobile">
-                        <?php
-                        if ($query_product->have_posts()) {
-                            while ($query_product->have_posts()) {
-                                $query_product->the_post();
-                                get_template_part('templates/components/cards/card', 'post-small');
-                            }
-                        }
-                        wp_reset_postdata();
-                        ?>
-                    </div>
+
 
                 </div>
             <?php endif ?>
@@ -97,20 +74,7 @@ $query_product = new WP_Query($query_product_args);
 
                         ?>
                     </div>
-                    <div class="blog-mobile">
-                        <?php
 
-                        if ($query_blog->have_posts()) {
-                            while ($query_blog->have_posts()) {
-                                $query_blog->the_post();
-
-                                get_template_part('templates/components/cards/card', 'post-small');
-                            }
-                        }
-                        wp_reset_postdata();
-
-                        ?>
-                    </div>
                 </div>
 
             <?php endif ?>
@@ -126,9 +90,6 @@ $query_product = new WP_Query($query_product_args);
             </div>
         <?php endif ?>
     </div>
-    <section>
 
-
-    </section>
 </main>
 <?php get_footer() ?>
